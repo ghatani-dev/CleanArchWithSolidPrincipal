1fee697c8db933ea569548d60646de7f458e531c
ujjwalsingh <ujjwalsingh@UJJWAL-PC>
2024-09-28 13:04:55 +0530

    First commit

--- CleanArchWithSolidPrincipal.Presentation/Program.cs
+++ CleanArchWithSolidPrincipal.Presentation/Program.cs
@@ -16,6 +16,7 @@ builder.Services.AddInfrastructureService(builder.Configuration);
 
 var app = builder.Build();
 
+
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
